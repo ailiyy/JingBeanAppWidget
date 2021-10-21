947545d85049242d14730749918dd9dd0b2e31fd
wangjing <1029@qq.com>
2021-10-21 10:11:31 +0800

    move

---
+++ app/src/main/java/com/wj/jd/dialog/MyMenuDialogAdapter.kt
@@ -0,0 +1,37 @@
+package com.wj.jd.dialog
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.wj.jd.R
+
+/**
+ * author wangjing
+ * Date 2021/10/21
+ * Description
+ */
+class MyMenuDialogAdapter(val activity: Activity) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var dataList: ArrayList<String>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MenuItem(LayoutInflater.from(activity).inflate(R.layout.menu_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return if (dataList == null) {
+            0
+        } else {
+            dataList!!.size
+        }
+    }
+
+    inner class MenuItem(view: View) : RecyclerView.ViewHolder(view) {
+        var title: TextView = view.findViewById(R.id.title)
+    }
+}
