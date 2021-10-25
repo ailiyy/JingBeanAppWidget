07f9505f4ebebf53bb16d4fc03826dc1e40668c5
wangjing <1029@qq.com>
2021-10-25 11:26:21 +0800

    move

---
+++ app/src/main/java/com/wj/jd/util/Main.kt
@@ -0,0 +1,19 @@
+package com.wj.jd.util
+
+import android.graphics.Color
+import android.util.Log
+
+/**
+ * author wangjing
+ * Date 2021/10/25
+ * Description
+ */
+fun main() {
+    colorToHexValue()
+}
+
+fun colorToHexValue() {
+    val d = Integer.valueOf("ff", 16)
+    Log.i("====", d.toString())
+}
+
